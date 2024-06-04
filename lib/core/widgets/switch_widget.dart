63e23d99ad0a7455977cc974976887fea0fac1c7
ReemBabelli <reembabelli793@gmail.com>
2024-06-04 12:46:56 +0300

    fix themeSwitchWidget and SwitchWidget

--- lib/core/widgets/switch_widget.dart
+++ lib/core/widgets/switch_widget.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:laza_app/core/theme/cubit/theme_cubit.dart';
-import 'package:laza_app/core/theme/cubit/theme_states.dart';
 import 'package:laza_app/core/theme/theme_utilities/colors.dart';
 
 class SwitchWidget extends StatefulWidget {
@@ -12,34 +9,17 @@ class SwitchWidget extends StatefulWidget {
 }
 
 class _SwitchWidgetState extends State<SwitchWidget> {
-
-
+  bool mode = false;
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<ThemeCubit, ThemeStates>(
-      builder: (context, state) {
-        if(state is SwitchThemeState){
-          bool mode = state.themeMode == ThemeMode.dark ? true : false;
-          return Switch(
-              activeTrackColor: mainColor,
-              value: mode,
-              onChanged: (value) {
-                setState(() {
-                  mode = value;
-                  if (value) {
-                    context.read<ThemeCubit>()
-                      .cacheTheme("dark");
-                  } else {
-                    context.read<ThemeCubit>()
-                      .cacheTheme("light");
-                  }
-                });
-              });
-        }
-        else {
-          return Container();
-        }
-      },
-    );
+
+    return Switch(
+        activeTrackColor: mainColor,
+        value: mode,
+        onChanged: (value) {
+          setState(() {
+            mode = value;
+          });
+        });
   }
 }
--- lib/features/products/presentation/widgets/side_bar_widget.dart
+++ lib/features/products/presentation/widgets/side_bar_widget.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:laza_app/core/theme/cubit/theme_cubit.dart';
+import 'package:laza_app/core/theme/cubit/theme_states.dart';
+import 'package:laza_app/core/theme/theme_utilities/colors.dart';
 import 'package:laza_app/core/theme/theme_utilities/styles.dart';
 import 'package:laza_app/core/utilities/router2.dart';
 import 'package:laza_app/core/utilities/size_config.dart';
@@ -59,7 +62,7 @@ class _SideBarWidgetState extends State<SideBarWidget> {
                   ? Images.sunWhite
                   : Images.sunBlack,
               text: 'Dark mode',
-              suffix: SwitchWidget(),
+              suffix: buildThemeSwitch(),
               onPressed: () {},
             ),
             const VerticalSpace(2),
@@ -135,6 +138,34 @@ class _SideBarWidgetState extends State<SideBarWidget> {
           ],
         ),
       ),
+    );
+  }
+
+  buildThemeSwitch() {
+    return  BlocBuilder<ThemeCubit, ThemeStates>(
+      builder: (context, state) {
+        if(state is SwitchThemeState){
+          bool mode = state.themeMode == ThemeMode.dark ? true : false;
+          return Switch(
+              activeTrackColor: mainColor,
+              value: mode,
+              onChanged: (value) {
+                setState(() {
+                  mode = value;
+                  if (value) {
+                    context.read<ThemeCubit>()
+                        .cacheTheme("dark");
+                  } else {
+                    context.read<ThemeCubit>()
+                        .cacheTheme("light");
+                  }
+                });
+              });
+        }
+        else {
+          return Container();
+        }
+      },
     );
   }
 }
