18742a5ee6d33808c1289630232efc508a9e95ce
ReemBabelli <reembabelli793@gmail.com>
2024-06-08 17:18:57 +0300

    add redirection in splash_screen.dart

--- lib/core/screens/splash_screen.dart
+++ lib/core/screens/splash_screen.dart
@@ -2,8 +2,9 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:laza_app/core/utilities/router2.dart';
+import 'package:laza_app/features/auth/data/data_sources/user_local_data_source.dart';
 import '../../resources/resources.dart';
-
+import 'package:laza_app/injection_container.dart' as di;
 
 
 class SplashScreen extends StatelessWidget {
@@ -11,8 +12,14 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Timer(const Duration(seconds: 3), () {
-      GoRouter.of(context).goNamed(MyRouter2.starter);
+    Timer(const Duration(seconds: 5), ()async {
+      final isLoggedIn =await di.sl<UserLocalDataSource>().isLoggedIn();
+      if(isLoggedIn){
+        GoRouter.of(context).goNamed(MyRouter2.home);
+      }else
+      {
+        GoRouter.of(context).goNamed(MyRouter2.starter);
+      }
       // context.goNamed(MyRouter2.starter);
     });
     return Scaffold(
